80713cb7c0181c967f7fd6fb99d7741639af9c82
murilojb <123109160+murilojb@users.noreply.github.com>
2023-08-03 15:08:34 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function setup() {
+    canvas=createCanvas(700, 500)
+    canvas.center()
+    background("white")
+    canvas.mouseReleased(classifyCanvas);
+    synth=window.speechSynthesis
+}
+function clearCanvas() {
+    background("white")
+}
+function draw() {
+    strokeWeight(13);
+    stroke(0)
+    if (mouseIsPressed) {
+        line(pmouseX,pmouseY,mouseX,mouseY)
+        
+    }
+}
+function preload() {
+    classifier=ml5.imageClassifier('DoodleNet')
+}
+function classifyCanvas() {
+    classifier.classify(canvas,gotResult)
+}
+function gotResult(error,results) {
+    if (error) {
+  console.error(error)      
+    }
+    console.log(results)
+    var result = results[0].label
+    document.getElementById('label').innerHTML = 'Nome: ' + result.replace('_', ' ');  
+    document.getElementById('confidence').innerHTML = 'Precisão: ' + Math.round(results[0].confidence * 100) + '%';
+    utterThis = new SpeechSynthesisUtterance(result.replace('_', ' '));
+    synth.speak(utterThis)
+
+}
